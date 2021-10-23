9b683fdf411fd931e2564bde7ecac5924b79ad45
andrea <kjkandrea@gmail.com>
2021-10-23 16:24:05 +0900

    Add lodash chain answer

--- src/index.js
+++ src/index.js
@@ -21,12 +21,23 @@ const { places } = generateFishingPlace()
  * @QUIZ : 2,200cm 이상의 🐋 3마리 잡기
  */
 
-// Array chain
-const fishing = places =>
+// Array method chain
+const fishing1 = places =>
   places
     .flatMap(({ fishList }) => fishList)
     .filter(({ fish }) => fish === '🐋')
     .filter(({ cm }) => 2200 <= cm)
     .splice(0, 3)
 
-logger('array chain', fishing.bind(null, places))
+logger('Array method chain', fishing1.bind(null, places))
+
+// Lodash chain
+import _ from 'lodash'
+const fishing2 = places =>
+    _.chain(places)
+      .flatMap(({ fishList }) => fishList)
+      .filter(({ fish }) => fish === '🐋')
+      .filter(({ cm }) => 2200 <= cm)
+      .take(3)
+      .value()
+logger('Lodash chain', fishing2.bind(null, places))
