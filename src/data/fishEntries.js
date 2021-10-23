9f9a299206a9dc0c154780db100211dd3f67dad8
andrea <kjkandrea@gmail.com>
2021-10-23 14:18:26 +0900

    Add generating data

---
+++ src/data/fishEntries.js
@@ -0,0 +1,20 @@
+const fishEntries = [
+  {
+    fish: '🐡',
+    percentage: 60,
+  },
+  {
+    fish: '🐠',
+    percentage: 25.15,
+  },
+  {
+    fish: '🦑',
+    percentage: 14.8,
+  },
+  {
+    fish: '🐋',
+    percentage: 0.05,
+  },
+]
+
+export default fishEntries
